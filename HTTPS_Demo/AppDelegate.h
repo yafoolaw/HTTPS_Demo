d5390f05dc7d258bf1c25581ecfeebc4b75c3a21
刘大帅 <FrankLiu@frankliu.local>
2015-12-08 11:30:33 +0800

    Initial Commit

---
+++ HTTPS_Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HTTPS_Demo
+//
+//  Created by FrankLiu on 15/12/8.
+//  Copyright © 2015年 FrankLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HTTPS_Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HTTPS_Demo
+//
+//  Created by FrankLiu on 15/12/8.
+//  Copyright © 2015年 FrankLiu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
