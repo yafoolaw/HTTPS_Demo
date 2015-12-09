3e6aa4b574b03b910a223e9a274b7f341da268b6
yafoolaw <yafoolaw@126.com>
2015-12-09 15:46:10 +0800

    提交

--- HTTPS_Demo/ViewController.m
+++ HTTPS_Demo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 
 @interface ViewController ()
 
@@ -16,12 +17,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    [self networking];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)networking {
+    
+    NSString * appleStr = @"https://www.apple.com/";
+    
+    NSString * kyfwStr = @"https://kyfw.12306.cn/otn/";
+    
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    
+    manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    
+    // 使用自签名证书要用一下代码来验证,其他情况要注掉
+//    manager.securityPolicy      = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModeCertificate];
+//    manager.securityPolicy.validatesDomainName      = YES;
+//    manager.securityPolicy.allowInvalidCertificates = YES;
+    
+    [manager GET:appleStr parameters:nil success:^(AFHTTPRequestOperation * _Nonnull operation, id  _Nonnull responseObject) {
+        
+        NSLog(@"****JSON: %@", responseObject);
+        
+    } failure:^(AFHTTPRequestOperation * _Nullable operation, NSError * _Nonnull error) {
+        
+        NSLog(@"****Error: %@", error);
+    }];
 }
 
 @end
